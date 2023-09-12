4ab6088263634e101e92089a2fb60030b4b15e25
iiriszhao <143477473+iiriszhao@users.noreply.github.com>
2023-09-12 14:18:48 +0800

    Yay

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = add(1,2);
+        System.out.println(resultOne);
+
+        String resultTwo = greeting("Mr Ewing");
+        System.out.println(resultTwo);
+
+        int resultThree = add(1,2,3,4);
+        System.out.println(resultThree);
+    }
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+    public static String greeting(String name) {
+        return "Bonjour, " + name + "!";
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        return add(add(numOne, numTwo),add(numThree, numFour));
+    }
+
+    public static void printMe(String toBePrinted) {
+        System.out.println(toBePrinted);
+    }
 }
